d24717b41d41f7bff95206ab13333082b57939ba
IrfanFaruk <irfanfaruk9@gmail.com>
2019-09-18 01:58:15 +0600

    for confict

--- app/src/main/java/com/example/mainapp/BlanksQuiz.java
+++ app/src/main/java/com/example/mainapp/BlanksQuiz.java
@@ -20,6 +20,11 @@ import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 
 public class BlanksQuiz extends AppCompatActivity {
+    /**
+     * first initialize the items that will be used in the blank quiz page.
+     * they are initialized private so that other page can't be able to use the items.
+     */
+
     private RadioGroup radioGroup;
     private RadioButton[] radioButtons;
     private TextView question;
@@ -36,49 +41,53 @@ public class BlanksQuiz extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blanks_quiz);
+        setContentView(R.layout.activity_blanks_quiz);                  //blank quiz activity is set for showing in the content
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+                                                                        //items are connected with the xml page items
         question=(TextView) findViewById(R.id.question);
         answertext=(EditText) findViewById((R.id.answertext));
         answer=(TextView) findViewById(R.id.answer);
         submit=(Button) findViewById(R.id.submit);
         next=(Button) findViewById(R.id.next);
-        next.setEnabled(false);
+        next.setEnabled(false);                                     //next button can't be used as long as it is enabled true
         check=(Button) findViewById(R.id.check);
 
-        Thread t1=new Thread(new ClientThread());
+        Thread t1=new Thread(new ClientThread());                   //for socket programming new thread is initialized
 
         t1.start();
-        try{
+        try{                                                        //the thread is kept in a try-catch block so that it can handle any exception occurred
             t1.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String []tokens=msg.split("#");
-        if(tokens[0].contains("200")){
-            questionno=tokens[1];
-            question.setText(tokens[1]+". "+tokens[2]);
-            str=tokens[3].trim();
+
+        String []tokens=msg.split("#");                     //message sent by the server are splitted by character '#' to use the required token
+        if(tokens[0].contains("200")){                             //0th token is sent '200' for correct sending information from the server
+            questionno=tokens[1];                                   //1st token is the question number
+            question.setText(tokens[1]+". "+tokens[2]);             //2nd token is the question itself, so it is showed by the text view
+            str=tokens[3].trim();                                   //3rd  token is the answer of the question.
 
         }
-        submit.setOnClickListener(new View.OnClickListener() {
+
+        submit.setOnClickListener(new View.OnClickListener() {      //this function is used for clicking the "Submit" button
             @Override
-            public void onClick(View v) {
-               if(answertext.getText().toString().trim().equalsIgnoreCase(str.trim())){
+            public void onClick(View v) {                           //if the button is clicked this view will be showed
+               if(answertext.getText().toString().trim().equalsIgnoreCase(str.trim())){         //which answer the user submitted is checked with the answer, if correct below statements will be run
                    answer.setTextColor(Color.GREEN);
                    answer.setText("Correct");
-                   next.setEnabled(true);
-                   check.setEnabled(false);
-                   submit.setEnabled(false);
+                   next.setEnabled(true);                           //Next button is enabled now
+                   check.setEnabled(false);                         //As the ans is correct, Check solution button is disabled
+                   submit.setEnabled(false);                        //Submit button is also disabled
                    isCorrect="true";
                }
-               else {
+               else {                                               //this section is for giving wrong answer
                    answer.setTextColor(Color.RED);
                    answer.setText("Wrong");
                    isCorrect="false";
                }
-               Thread t2=new Thread(new NextThread());
+
+               Thread t2=new Thread(new NextThread());              //new thread is created for another socket programming
                t2.start();
                try{
                    t2.join();
